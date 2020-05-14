f540c0d8783ab70c0972aa0c026a06d927079186
yaoshunqing <yaoshunqing0106@gmail.com>
2020-05-14 19:45:10 +0800

    'new'

---
+++ 4/bytes.py
@@ -0,0 +1,5 @@
+# encoding= utf-8  
+# @Time : 2020/5/14 14:16 
+# @Author : Yao
+# @File : bytes.py 
+# @Software: PyCharm
---
+++ 5/function.py
@@ -0,0 +1,5 @@
+# encoding= utf-8  
+# @Time : 2020/5/14 18:55 
+# @Author : Yao
+# @File : function.py 
+# @Software: PyCharm
